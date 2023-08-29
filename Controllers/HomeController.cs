2bce6705d66d2dcce19bd8c66e6cc34b8fab7c01
Daniel Haag <danni.haag91@gmail.com>
2023-08-28 23:35:07 -0300

    Inicio da implementação da agenda de piscineiros | Ajustes na tela de login

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,12 +19,31 @@ namespace PoolPiscinas.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int? month, int? year)
         {
-            //var teste = HttpContext.Session.GetString("Nome");
+            DateTime currentDate = DateTime.Now;
+
+            if (month.HasValue && year.HasValue)
+            {
+                currentDate = new DateTime(year.Value, month.Value, 1);
+
+                // Tratar casos onde o mês ou ano excedem os limites
+                if (currentDate.Month == 0)
+                {
+                    currentDate = currentDate.AddYears(-1).AddMonths(12);
+                }
+                else if (currentDate.Month == 13)
+                {
+                    currentDate = currentDate.AddYears(1).AddMonths(-12);
+                }
+            }
+
+            ViewBag.CurrentDate = currentDate;
 
             return View();
         }
+
+
 
         public IActionResult Contato()
         {
