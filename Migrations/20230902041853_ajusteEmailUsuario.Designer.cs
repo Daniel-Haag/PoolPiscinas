626477f1182ae52c1e3734ac2a8622cc8f558ee3
Daniel Haag <danni.haag91@gmail.com>
2023-09-02 01:19:53 -0300

    Adição email no modelo usuario

--- Migrations/20230902041853_ajusteEmailUsuario.Designer.cs
+++ Migrations/20230902041853_ajusteEmailUsuario.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PoolPiscinas.Models;
 
 namespace PoolPiscinas.Migrations
 {
     [DbContext(typeof(PoolPiscinasDbContext))]
-    partial class PoolPiscinasDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230902041853_ajusteEmailUsuario")]
+    partial class ajusteEmailUsuario
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -8,6 +8,7 @@ namespace PoolPiscinas.Models
         public int RoleID { get; set; }
         public Role Role { get; set; }
         public string Nome { get; set; }
+        public string Email { get; set; }
         public string CPF { get; set; }
         public string CNPJ { get; set; }
         public string Senha { get; set; }
